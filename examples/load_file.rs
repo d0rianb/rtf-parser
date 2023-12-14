6af1ab84d44fa07508e4026ab83844336980c802
d0rianb <dorian.beauchesne@icloud.com>
2023-12-14 14:27:53 +0100

    Update examples

--- examples/load_file.rs
+++ examples/load_file.rs
@@ -2,7 +2,7 @@ extern crate rtf_parser;
 use rtf_parser::{Lexer, Parser};
 
 fn main() {
-    let rtf_text = include_str!("/Users/dorian/Downloads/file-sample_1MB.rtf");
+    let rtf_text = include_str!("../resources/tests/test-file.rtf");
     let tokens = Lexer::scan(rtf_text);
     let doc = Parser::new(tokens).parse();
 }
