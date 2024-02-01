59f4a688511c07ba17eac7b5ea34d6478ad92711
d0rianb <dorian.beauchesne@icloud.com>
2024-02-01 15:20:23 +0100

    Update examples

--- examples/load_file.rs
+++ examples/load_file.rs
@@ -3,6 +3,6 @@ use rtf_parser::{Lexer, Parser};
 
 fn main() {
     let rtf_text = include_str!("../resources/tests/test-file.rtf");
-    let tokens = Lexer::scan(rtf_text);
+    let tokens = Lexer::scan(rtf_text).expect("Invalid RTF content");
     let doc = Parser::new(tokens).parse();
 }
