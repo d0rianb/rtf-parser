e32f51da013942c0d71657099d74c853f85c6582
d0rianb <dorian.beauchesne@icloud.com>
2024-04-26 19:47:53 +0200

    Add a bench script

--- examples/bench.rs
+++ examples/bench.rs
@@ -9,9 +9,9 @@ fn main() {
     {
         let rtf_text = include_str!("../resources/tests/file-sample_500kB.rtf");
         let tokens = Lexer::scan(rtf_text).expect("Invalid RTF content");
-        doc = Parser::new(tokens).parse();
+        doc = Parser::new(tokens).parse().unwrap();
     }
     let elapsed = start.elapsed();
-    assert_eq!(doc.unwrap().header.stylesheet, StyleSheet::new()); // in order to not get optimized out
+    assert_eq!(doc.header.stylesheet, StyleSheet::new()); // in order to not get optimized out
     println!("Elapsed: {:.2?}", elapsed);
 }
